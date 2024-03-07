78c144e147e80ba93676043fd0bad4a928b1af31
Alex&Indie <92387131+AlexIndiex@users.noreply.github.com>
2024-03-07 22:18:10 +0100

    Delete ninty emulators directory

--- ninty emulators/citrus emudeck middleware SRM/citra.ps1
+++
@@ -1,8 +0,0 @@
-$emulatorFile = "$env:USERPROFILE/EmuDeck/EmulationStation-DE/Emulators/citra/citra-qt.exe"
-$scriptFileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
-. "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/allCloud.ps1"
-if($args){
-	$formattedArgs = $args | ForEach-Object { '"' + $_ + '"' }
-}
-
-emulatorInit $scriptFileName $emulatorFile ($formattedArgs -join ' ')
