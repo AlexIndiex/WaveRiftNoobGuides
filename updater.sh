f8201315d0c24b8ca2a6eeb894463506e1770c35
Alex&Indie <92387131+AlexIndiex@users.noreply.github.com>
2024-04-15 02:12:52 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -57,7 +57,6 @@ download_notify() {
                                 chmod +x ~/Applications/mGBAdev.AppImage
                                 ;;
                         
-                        #suyu)
                         #        curl -L -o ~/Applications/suyu.AppImage -z ~/Applications/suyu.AppImage "${urls[0]}"
                         #        chmod +x ~/Applications/suyu.AppImage
                         #        ;;
