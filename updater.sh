f3f980782402ed4f6fd6c54a7e5676e7bc9d0488
Alex&Indie <92387131+AlexIndiex@users.noreply.github.com>
2024-04-15 02:07:00 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -90,7 +90,7 @@ download_notify() {
                         #        7z x Lemonade.zip lemonade*7z -y
                         #        mv ~/Applications/lemonade*7z ~/Applications/Lemonade.7z
                         #        7z x Lemonade.7z -o* -y
-                                chmod +x ~/Applications/Lemonade/head/lemonade.AppImage
+                        #        chmod +x ~/Applications/Lemonade/head/lemonade.AppImage
                                 chmod +x ~/Applications/Lemonade/head/lemonade-qt.AppImage
                                 chmod +x ~/Applications/Lemonade/head/lemonade-room.AppImage
                                 ;;
