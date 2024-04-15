b66868470dd62eea51c8859b4aa17cb19de29519
Alex&Indie <92387131+AlexIndiex@users.noreply.github.com>
2024-04-15 02:13:47 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -80,7 +80,6 @@ download_notify() {
                                 chmod +x ~/Applications/citraPMK7/head/citra-room.AppImage
                                 ;;
                         
-                        #        7z x Lemonade.zip lemonade*7z -y
                         #        mv ~/Applications/lemonade*7z ~/Applications/Lemonade.7z
                         #        7z x Lemonade.7z -o* -y
                         #        chmod +x ~/Applications/Lemonade/head/lemonade.AppImage
