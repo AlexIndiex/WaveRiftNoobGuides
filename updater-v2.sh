f0e865c1831be2b1a3ebb2569e704c7c1533853a
Alex&Indie <92387131+AlexIndiex@users.noreply.github.com>
2024-05-14 21:09:34 +0200

    Update updater-v2.sh

--- updater-v2.sh
+++ updater-v2.sh
@@ -12,8 +12,8 @@ download_notify() {
     case $app_name in
         Ryujinx)
             url=$(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/Ryujinx/release-channel-master/releases | \
-        jq -r '.[].assets[] | select(.browser_download_url | test("linux_x64")) | .browser_download_url')
-            file_name="Ryujinx-x64.tar.gz"
+                jq -r '.[].assets[] | select(.browser_download_url | test("linux_x64")) | .browser_download_url')
+            file_name="Ryujinx.tar.gz"
             ;;
         Cemu)
             url=$(curl -s -H "Accept: application/vnd.github+json" -G -d 'per_page=1' https://api.github.com/repos/cemu-project/Cemu/releases | \
